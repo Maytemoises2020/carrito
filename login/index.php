e90b50b56935d14108aee7421a62ff9d928579b1
Maytemoises2020 <anei.mayte7@gmail.com>
2023-09-06 12:26:38 -0500

    inicio

---
+++ login/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>VetMay</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="MyraStudio" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="../plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+ 
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex align-items-center min-vh-100">
+                        <div class="w-100 d-block bg-white shadow-lg rounded my-5">
+                            <div class="row">
+                                <div class="col-lg-6 d-none d-lg-block rounded-left">
+                                    <img src="../img/LOGO.jpeg" width="545x" height="500px" alt="">
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center mb-5">
+                                            <a href="index.html" class="text-dark font-size-22 font-family-secondary">
+                                                <i class="mdi"></i> <b>VETERINARIA MAY</b>
+                                            </a>
+                                        </div>
+                                        <h1 class="h5 mb-1">BIENVENIDO</h1>
+                                        <p class="text-muted mb-4">Ingrese usuario y contraseña para acceder.</p>
+                                        <form class="user">
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Usuario/Correo">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Contraseña">
+                                            </div>
+                                            <a href="" class="btn btn-info btn-block waves-effect waves-light"> Iniciar Sesión </a>
+
+                                           
+                                        </form>
+
+                                        <div class="row mt-4">
+                                            <div class="col-12 text-center">
+                                                <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1"><u>Olvide mi contraseña</u></a></p>
+                                            </div> <!-- end col -->
+                                        </div>
+                                        <!-- end row -->
+                                    </div> <!-- end .padding-5 -->
+                                </div> <!-- end col -->
+                            </div> <!-- end row -->
+                        </div> <!-- end .w-100 -->
+                    </div> <!-- end .d-flex -->
+                </div> <!-- end col-->
+            </div> <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end page -->
+
+    <!-- jQuery  -->
+    <script src="../plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
+    <script src="../plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
+    <script src="../plantilla/Admin/vertical/assets/js/waves.js"></script>
+    <script src="../plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
+
+    <!-- App js -->
+    <script src="assets/js/theme.js"></script>
+
+</body>
+
+</html>
